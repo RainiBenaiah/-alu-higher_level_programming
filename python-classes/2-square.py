63a10b3804b11b84513f23118f9801d26ef96e55
RainiBenaiah <b.raini@alustudent.com>
2023-07-25 12:19:52 -0700

    class2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,7 +5,7 @@
 class Square:
     '''Square class with private size attr'''
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         '''Initializes instances of Square.
 
            Args:
