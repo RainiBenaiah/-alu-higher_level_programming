8031a8dd7b235f7d5ae79270072651548d977afc
RainiBenaiah <b.raini@alustudent.com>
2023-08-04 12:42:03 -0700

    update

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
+"""Sends a POST request to a given URL with a given email.
+
+Usage: ./2-post_email.py <URL> <email>
+  - Displays the body of the response.
 """
-given URL & email as params, send POST req to URL, display response body utf-8
-usage: ./2-post_email.py http://0.0.0.0:5000/post_email hr@holbertonschool.com
-"""
-from sys import argv
+import sys
 import urllib.parse
 import urllib.request
 
 
 if __name__ == "__main__":
-    url = argv[1]
-    values = {'email': argv[2]}
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
 
-    data = urllib.parse.urlencode(values)
-    data = data.encode('ascii')
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
-        print(response.read().decode('utf-8'))
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
