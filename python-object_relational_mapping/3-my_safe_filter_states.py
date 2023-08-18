63d566b273c649425e84439702db0bf17b96f6e5
RainiBenaiah <b.raini@alustudent.com>
2023-08-18 03:33:04 -0700

    done

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,31 +1,17 @@
 #!/usr/bin/python3
-"""
-Script that lists all values in the `states` table of `hbtn_0e_0_usa`
-where `name` matches the argument `state name searched`.
-Arguments:
-    mysql username (str)
-    mysql password (str)
-    database name (str)
-    state name searched (str)
-"""
-
-import sys
+''' lists all states with a name matching with specific name
+ from the database hbtn_0e_0_usa (sql injection safe)
+'''
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    mySQL_u = sys.argv[1]
-    mySQL_p = sys.argv[2]
-    db_name = sys.argv[3]
-
-    searched_name = sys.argv[4]
-
-    # By default, it will connect to localhost:3306
-    db = MySQLdb.connect(user=mySQL_u, passwd=mySQL_p, db=db_name)
-    cur = db.cursor()
-
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id"
-                .format(searched_name))
-    rows = cur.fetchall()
-
-    for row in rows:
+    db = MySQLdb.connect(host="localhost", port=3306, db=sys.argv[3],
+                         user=sys.argv[1], passwd=sys.argv[2])
+    c = db.cursor()
+    c.execute(
+        "SELECT * FROM states WHERE states.name = %s ORDER BY states.id ASC",
+        (sys.argv[4],)
+    )
+    for row in c.fetchall():
         print(row)
