9b58a84a0bbc1a016f718ff66e1b73632470431e
RainiBenaiah <b.raini@alustudent.com>
2023-09-30 12:25:05 -0700

    pose

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const words = [“C is fun”,  “Python is cool”,  “JavaScript is amazing”];
-for (const word of words) {
-  console.log(word\n);
+
+for ( let a = 0; a < words.length; a++) { 
+	console.log(words[a]);
 }
