9522a4e52e8ca57cb52754d85dbbcf1260b9c26f
RainiBenaiah <b.raini@alustudent.com>
2023-09-30 11:28:57 -0700

    process

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-if (process.argv[1]) {
-	console.log(process.argv[1]);
-} else { console.log( 'No argument');
+if (process.argv[2]) {
+	console.log(process.argv[2]);
+} else { 
+  console.log('No argument');
 }
