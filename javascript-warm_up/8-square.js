62c1d2d876ef3dc9bcafd2025159cdb252de336a
RainiBenaiah <b.raini@alustudent.com>
2023-09-30 13:21:31 -0700

    square

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 const size = parseInt(process.argv[2]);
-
-  if (isNaN(size)) {
-    console.log('Missing size');
-  } else {
-    for (let a = 0; a < size; a++) {
-      let row = '';
-      for (let B = 0; B < size; B++) {
-        row += 'X';
-      }
-      console.log(row);
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let a = 0; a < size; a++) {
+    let row = '';
+    for (let b = 0; b < size; b++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
