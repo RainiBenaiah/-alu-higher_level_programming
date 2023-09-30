5180e1dce06a5dbd972f54f1d9bc1cbc468e0789
RainiBenaiah <b.raini@alustudent.com>
2023-09-30 13:33:21 -0700

    add

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const a = parseInt(process.argv[2];
-const b = parseInt(process.argv[3];
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
 const sum = a + b;
 console.log(sum);
