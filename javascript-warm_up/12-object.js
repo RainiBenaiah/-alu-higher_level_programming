3e73c00c3acf793d96f370ba7f5903b52e441041
RainiBenaiah <b.raini@alustudent.com>
2023-09-30 15:41:07 -0700

    object

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const original = 12;
-if (original === 12) {
-  const updated = 89;
-  console.log(updated);
-} else {
-  console.log(original);
-}
-console.log(original);
+const oobject = {
+  type: 'object',
+  value: 12
+};
+console.log(oobject);
+myObject.value = 89;
+console.log(oobject);
