8e67a980422d58b163693c3e4438aa4755dd6828
RainiBenaiah <b.raini@alustudent.com>
2023-10-07 17:21:00 -0700

    man

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const fs = require('fs')
+const fs = require('fs');
 fs.readFile(process.argv[2], (errow, paragraph) => {
    if (errow) throw errow;
-      console.log(paragraph);
-})
+      console.log(paragraph.toString());
+});
